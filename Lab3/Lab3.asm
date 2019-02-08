566c1d11fd8c961c48df39a142a71c49f3f75c90
Samuel English <sdenglis@ucsc.edu>
2019-02-07 22:50:30 -0800

    Started organizing thoughts with comments

---
+++ Lab3/Lab3.asm
@@ -0,0 +1,27 @@
+##########################################################################
+# Created by: English, Samuel
+# sdenglis
+# 2 February 2019
+#
+# Assignment: Lab 3: MIPS Looping ASCII Art
+# CMPE 012, Computer Systems and Assembly Language
+# UC Santa Cruz, Winter 2019
+#
+# Description: This program prints ASCII-based triangles to the screen.
+#
+# Notes: This program is intended to be run from the MARS IDE.
+##########################################################################
+
+
+
+.data # Initiates and stores variables / int's in RAM.
+
+
+      # Enter the length of one of the triangle legs: _
+      # Enter the number of triangles to print: _
+
+
+.text # Calls variables during stack in form of functions and arguments.
+
+
+      # Print combination of inputs and character variables based on user feedback
