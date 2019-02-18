4d346c96810dacf5bf89ab8c65c2402d1bb47591
Samuel English <sdenglis@ucsc.edu>
2019-02-18 13:24:43 -0800

    Uploaded Lab4.asm for desktop access.

---
+++ Lab4/Lab4.asm
@@ -0,0 +1,24 @@
+##########################################################################
+# Created by:  English, Samuel
+#              sdenglis
+#              7 August 2018
+# Assignment:  Lab 4: ASCII conversion
+#              CMPE 012, Computer Systems and Assembly Language
+#              UC Santa Cruz, Winter 2019
+# Description: This program converts user ASCII string input into a base(4) integer
+# Notes:       This program is intended to be run from the MARS IDE.
+##########################################################################
+# REGISTER USAGE
+# $ra: Return to caller when fitting procedures into code.
+# $s0: Stores summation of $s1 & $s2 values.
+# $s1: Holds 32-bit 2SC of first value.
+# $s2: Holds 32-bit 2SC of second value.
+# $t0: Temporary Register.
+# $t1: Temporary Register.
+# $v0: Syscall Register.
+# $a0: Arguments register.
+
+.data
+
+.text
+
