9702249ba830f6e65bdcdc0e9de38349d557a1e7
Samuel English <sdenglis@ucsc.edu>
2019-03-09 18:28:08 -0800

    discovered a way to actually shift the inputString the correct amount!

--- Lab5/Lab5.asm
+++ Lab5/Lab5.asm
@@ -12,6 +12,10 @@
 	operationArray:    .space 100   # single character reserved for E, D, X?
 	
 	invalidInput:      .asciiz      "Invalid input: please input E, D, or X."
+	upperAl:           .asciiz      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowerAl:           .asciiz      "abcdefghijklmnopqrstuvwxyz"
+	Encrypted:         .asciiz      "<Encrypted> "
+	Decrypted:         .asciiz      "<Decrypted> "
 
 .text
 
@@ -103,9 +107,17 @@ cipher: nop
 	sw   $ra, ($sp)              # push $ra to the stack
 	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
 	sw   $a1, ($sp)              # push $a1 to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $a2, ($sp)              # push $a2 to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $s2, ($sp)              # push $s2 to the stack
 	
-	jal  compute_checksum        # jump to compute_checksum function
+	jal  compute_checksum        # jump to compute_checksum function, store value in $v0
 	
+	lw   $s2, ($sp)              # pop stack, obtain previously saved $s2 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
+	lw   $a2, ($sp)              # pop stack, obtain previously saved $a2 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
 	lw   $a1, ($sp)              # pop stack, obtain previously saved $a1 value
 	addu $sp,  $sp, 4            # increment stack pointer by 4
 	lw   $ra, ($sp)              # pop stack, obtain previously saved $ra value
@@ -116,9 +128,21 @@ cipher: nop
 bne  $t0,  68, skipDecrypt           # branch to next case if $t0 does not contain 'D'
 	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
 	sw   $ra, ($sp)              # push $ra to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $a1, ($sp)              # push $a1 to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $a2, ($sp)              # push $a2 to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $s2, ($sp)              # push $s2 to the stack
 	
-	jal  decrypt
+	jal  decrypt                 # jump to decrypt function
 	
+	lw   $s2, ($sp)              # pop stack, obtain previously saved $s2 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
+	lw   $a2, ($sp)              # pop stack, obtain previously saved $a2 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
+	lw   $a1, ($sp)              # pop stack, obtain previously saved $a1 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
 	lw   $ra, ($sp)              # pop stack, obtain previously saved $ra value
 	addu $sp,  $sp, 4            # increment stack pointer by 4
 	
@@ -128,13 +152,26 @@ skipDecrypt: nop
 # no need for branch statement since only two possible outcomes (Decrypt, Encrypt)
 	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
 	sw   $ra, ($sp)              # push $ra to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $a1, ($sp)              # push $a1 to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $a2, ($sp)              # push $a2 to the stack
+	subu $sp,  $sp, 4            # save previous caller address before jumping to nested subroutine
+	sw   $s2, ($sp)              # push $s2 to the stack
 	
-	jal  encrypt
+	jal  encrypt                 # jump to encrypt function
 	
+	lw   $s2, ($sp)              # pop stack, obtain previously saved $s2 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
+	lw   $a2, ($sp)              # pop stack, obtain previously saved $a2 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
+	lw   $a1, ($sp)              # pop stack, obtain previously saved $a1 value
+	addu $sp,  $sp, 4            # increment stack pointer by 4
 	lw   $ra, ($sp)              # pop stack, obtain previously saved $ra value
 	addu $sp,  $sp, 4            # increment stack pointer by 4
 	
 endCipher: nop
+	jr $ra
 
 
 
@@ -174,13 +211,32 @@ encrypt: nop
 # Uses a Caesar cipher to encrypt a character using the key returned from 
 # compute_checksum. This function should call check_ascii.
 #
-# arguments:  $a0 - character to encrypt 
+# arguments:  $a2 - character to encrypt 
 #             $a1 - checksum result
 #
 # return:     $v0 - encrypted character 
 #-------------------------------------------------------------------- 
 
+	move $a1,  $v0                 # sets $a1 to the checksum result generated in XOR function
+encryptLoop:
+	lb   $a2, ($s2)                # stores character to encrypt into $a2
+beq     $a2, 0,    exitEncrypt         # exit loop when we reach a null terminator
+	
+	subu $sp,  $sp, 4              # save previous caller address before jumping to nested subroutine
+	sw   $ra, ($sp)                # push $ra to the stack
+	
+	jal check_ascii                # sort ascii characters and convert accordingly
+	
+	lw   $ra, ($sp)                # pop stack, obtain previously saved $ra value
+	addu $sp,  $sp, 4              # increment stack pointer by 4
+	
+	sb   $a2,                      # store shifted character into final array structure
+	addi $s2, $s2, 1               # add to address pointer for inputString
+	j encryptLoop                  # repeat
 
+
+exitEncrypt: nop
+	jr $ra                         # return to cipher command address
 
 decrypt: nop
 #-------------------------------------------------------------------- 
@@ -189,7 +245,7 @@ decrypt: nop
 # Uses a Caesar cipher to decrypt a character using the key returned from 
 # compute_checksum. This function should call check_ascii
 #
-# arguments:  $a0 - character to decrypt 
+# arguments:  $a2 - character to decrypt 
 #             $a1 - checksum result
 #
 # return:     $v0 - decrypted character 
@@ -204,11 +260,49 @@ check_ascii: nop
 # This checks if a character is an uppercase letter, lowercase letter, or  
 # not a letter at all. Returns 0, 1, or -1 for each case, respectively.
 #
-# arguments:  $a0 - character to chec
+# arguments:  $a0 - character to check
 #
 # return:     $v0 - 0 if uppercase, 1 if lowercase, -1 if not letter
 #-------------------------------------------------------------------- 
 
+checkUpper: nop                   # check for upper-case value
+bge    $a2, 65,   upper           # check if loaded character is above 'A'
+	j elseASCII               # else, non-letter character
+upper: nop
+ble    $a2, 90,   upperTrue       # check if loaded character is below 'Z'
+	j checkLower              # else, check if lower-case value
+
+upperTrue: nop                    # function for upper-case value
+	sub  $t9, $a2, 65         # subtract ascii character from base ascii value
+	add  $t9, $t9, $a1        # adds temporary value to XOR shift amount
+	abs  $t9, $t9             # ensures that we divide by a positive
+	divu $t9, $t9, 26         # divide temp value by 26
+	mfhi $t8                  # store mod value into $t8
+	addi $a2, $t8, 65         # add mod value to base ascii value, store back into $a2
+	
+	j elseASCII               # end process and continue
+
+checkLower: nop                   # check for lower-case value
+bge    $a2, 97,   lower           # check if loaded character is above 'a'
+	j elseASCII               # else, non-letter character
+lower: nop
+ble    $s2, 122,  lowerTrue       # check if loaded character is below 'z'
+	j elseASCII               # else, non-letter character
+
+lowerTrue: nop                    # function for lower-case value
+	sub  $t9, $a2, 97         # subtract ascii character from base ascii value
+	add  $t9, $t9, $a1        # adds temporary value to XOR shift amount
+	abs  $t9, $t9             # ensures that we divide by a positive
+	divu $t9, $t9, 26         # divide temp value by 26
+	mfhi $t8                  # store mod value into $t8
+	addi $a2, $t8, 97         # add mod value to base ascii value, store back into $a2
+	
+	j elseASCII               # end process and continue
+
+elseASCII: nop                    # else, treat as non-letter value
+	li $t9, 0                 # clear up register space
+	li $t8, 0
+	jr $ra
 
 
 print_strings: nop
@@ -226,12 +320,47 @@ print_strings: nop
 # return:     prints to console 
 #-------------------------------------------------------------------- 
 
+	lb   $t0, ($s0)	               # load X, D, E character
+beq  	$t0, 68, DecryptedCase         # check if program decrypted string
+beq  	$t0, 69, EncryptedCase         # check if program encrypted string
 
+DecryptedCase: nop
+	la $a0, Encrypted              # <Encrypted> 
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
+	
+	la $a0, $s3                    # <Encrypted> string
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
+	
+	la $a0, Decrypted              # <Decrypted> 
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
 
+	la $a0, $s2                    # <Decrypted> string
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
+	j  exitPrint                   # skip next case 
+	
+EncryptedCase: nop
+	la $a0, Encrypted              # <Encrypted> 
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
+	
+	la $a0, $s3                    # <Encrypted> string
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
+	
+	la $a0, Decrypted              # <Decrypted> 
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
 
-
-
-
+	la $a0, $s2                    # <Decrypted> string
+	li $v0, 4                      # prints prompt loaded into $a0
+	syscall                        # execute command
+	
+exitPrint: nop
+	jr $ra                         # return to caller address
 
 
 
